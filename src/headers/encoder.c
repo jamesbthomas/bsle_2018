8b4e2951ab84b5e91c81f99a9eb0805050d34d47
jamesbthomas <james.b.thomas@mac.com>
2018-03-22 11:08:20 -0400

    encode ready, needs tests

--- src/headers/encoder.c
+++ src/headers/encoder.c
@@ -16,24 +16,21 @@ typedef struct Patterns {
 	int * lens;
 } Pattern;
 
-// Function to validate an encoding pattern scraped from the C2's request packet
-// Returns a struct containing the parsed pattern or none if invalid
-Pattern * patternValidate(char * pattern){
-	Pattern * parsed = calloc(1,sizeof(Pattern));
+// Function to validate an encoding pattern scraped from the C2's request packet and parse it for easier use later
+// Takes the scraped pattern as a string and the Pattern object that will store the parsed pattern
+// Returns 0 if successful, 1 if failed
+int patternValidate(char * pattern,Pattern * parsed){
 	parsed -> numOpts = 0;
 	parsed -> maxOpts = 2;
 	parsed -> opts = calloc(parsed->maxOpts,sizeof(char *));
-	parsed -> ops = calloc(parsed->maxOpts,sizeof(char *));
-	parsed -> vals = calloc(parsed->maxOpts,sizeof(int));
-	parsed -> lens = calloc(parsed->maxOpts,sizeof(int));
 	char * token = strtok(pattern,";");
 	while (token != NULL){
 		if (parsed->numOpts > parsed->maxOpts){
 			parsed->maxOpts = parsed->maxOpts * 2;
 			parsed->opts = (char **) realloc(parsed->opts,parsed->maxOpts);
-			if (parsed->opts == NULL){
+			if (parsed->opts == NULL){ // | (parsed->ops == NULL) | (parsed->vals == NULL) | (parsed->lens == NULL)){
 				printf("Error: Realloc Failure\n");
-				exit(2);
+				return 1;
 			}
 		}
 		parsed->opts[parsed->numOpts] = calloc(strlen(token),sizeof(char));
@@ -43,22 +40,25 @@ Pattern * patternValidate(char * pattern){
 	}
 	if (parsed->numOpts < 2){
 		printf("Error: Invalid Pattern\n");
-		exit(2);
+		return 1;
 	}
 	int i;
 	char * opVal;
+	parsed->ops = (char *) calloc(parsed->numOpts,sizeof(char *));
+	parsed->vals = (int *) calloc(parsed->numOpts,sizeof(int));
+	parsed->lens = (int *) calloc(parsed->numOpts,sizeof(int));
 	for (i = 0;i < parsed->numOpts;i++){
-		opVal = strtok(parsed->opts[i],":");
+		opVal = strtok(strdup(parsed->opts[i]),":");
 		char * len = strtok(NULL,":");
 		int num = atoi(len);
 		if (num == 0 && strncmp(len,"0",1) != 0){
 			printf("Error: Invalid Length\n");
-			exit(2);
+			return 1;
 		}
 		parsed->lens[i] = num;
 		if (strtok(NULL,":") != NULL){
 			printf("Error: Invalid Option\n");
-			exit(2);
+			return 1;
 		}
 		if (strncmp(opVal,"^",1) == 0){
 			parsed->ops[i] = opVal[0];
@@ -66,12 +66,11 @@ Pattern * patternValidate(char * pattern){
 			for (int c = 1;c < strlen(opVal);c++){
 				if (isdigit(opVal[c]) == 0){
 					printf("Error: Invalid Value\n");
-					exit(2);
+					return 1;
 				}
 				val[c-1] = opVal[c];
 			}
 			parsed->vals[i] = atoi(val);
-			free(val);
 		}
 		else if (strncmp(opVal,"ror",3) == 0){
 			parsed->ops[i] = opVal[2];
@@ -79,46 +78,99 @@ Pattern * patternValidate(char * pattern){
 			for (int c = 3;c < strlen(opVal);c++){
 				if (isdigit(opVal[c]) == 0){
 					printf("Error: Invalid Value\n");
-					exit(2);
+					return 1;
 				}
 				val[c-3] = opVal[c];
 			}
 			parsed->vals[i] = atoi(val);
-			free(val);
 		}
 		else if (strncmp(opVal,"rol",3) == 0){
 			parsed->ops[i] = opVal[2];
 			char * val = calloc(strlen(opVal)-3,sizeof(char));
 			for (int c = 3;c < strlen(opVal);c++){
 				if (isdigit(opVal[c]) == 0){
-					printf("Errpr: Invalid Value\n");
-					exit(2);
+					printf("Error: Invalid Value\n");
+					return 1;
 				}
 				val[c-3] = opVal[c];
 			}
 			parsed->vals[i] = atoi(val);
-			free(val);
 		}
 		else if (strncmp(opVal,"~",1) == 0){
 			parsed->ops[i] = opVal[0];
-			parsed->vals[i] = -1;
+			parsed->vals[i] = 0;
 		}
 		else{
 			printf("Error: Invalid Operation\n");
 			exit(2);
 		}
 	}
-	for (int i = 0;i < parsed->numOpts;i++){
-		printf("%d\t",i);
-		printf("%s\t%c\t%d\t%d\n",parsed->opts[i],parsed->ops[i],parsed->vals[i],parsed->lens[i]);
+	return 0;
+}
+
+// AUTHOR NAME: dasblinkenlight (stackoverflow username)
+// WEB ADDRESS: https://ideone.come/pq4Ejs - linked from https://stackoverflow.com/questions/21289606/rotation-of-unsigned-char-by-n-bits
+// DATE ACCESSED: 28MAR2018
+// Altered slightly to match what I needed
+unsigned char ror(const unsigned char value, int shift){
+	return (value >> shift & 0xFF) | (value << (8 - shift) & 0xFF);
+}
+
+// AUTHOR NAME: dasblinkenlight (stackoverflow username)
+// WEB ADDRESS: https://ideone.come/pq4Ejs - linked from https://stackoverflow.com/questions/21289606/rotation-of-unsigned-char-by-n-bits
+// DATE ACCESSED: 28MAR2018
+// Altered slightly to match what I need
+unsigned char rol(const unsigned char value, int shift){
+	return (value << shift & 0xFF) | (value >> (8 - shift) & 0xFF);
+}
+
+// Function to encode a string using a provided parsed encoding pattern
+// Returns encoded string if successfull, NULL otherwise
+unsigned char * encode(char * data,Pattern * parsed){
+	int currOpt = 0;
+	int size = strlen(data)-1;
+	printf("%d\n",size);
+	int done = 0;
+	unsigned char * encoded = calloc(size,sizeof(unsigned char));
+	while (currOpt < parsed->numOpts){
+		char opt = parsed->ops[currOpt];
+		int val = parsed->vals[currOpt];
+		int len = parsed->lens[currOpt];
+		for (int count = 0;count < len;count++){
+			printf("%d\t%c\t%c\n",done,data[done],opt);
+			if (done > size){
+				return encoded;
+			}
+			if (opt == '^'){
+				encoded[done] = val ^ data[done];
+			}
+			else if (opt == 'r'){
+				encoded[done] = ror(data[done],val);
+			}
+			else if (opt == 'l'){
+				encoded[done] = rol(data[done],val);
+			}
+			else if (opt == '~'){
+				encoded[done] = ~data[done];
+			}
+			done += 1;
+		}
+		currOpt += 1;
 	}
-	return parsed;
+	return NULL;
 }
 
 #endif
 // Main function for interacting directly with this module
 int main(int argc,char ** argv){
 	char *data = strdup("ror1:2;^27:2;~:180;rol50:34");
-	patternValidate(data);
+	Pattern * parsed = calloc(1,sizeof(Pattern));
+	patternValidate(data,parsed);
+	for (int i = 0;i < parsed->numOpts;i++){
+		printf("%d\t",i);
+		printf("%s\t%c\t%d\t%d\n",parsed->opts[i],parsed->ops[i],parsed->vals[i],parsed->lens[i]);
+	}
+	printf("\n");
+	printf("%s\n",encode("message",parsed));
 	return 0;
 }
